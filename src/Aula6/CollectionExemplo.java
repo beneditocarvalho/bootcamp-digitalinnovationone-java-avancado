a2a95adb87077d7751132b143c28ff00f23eb99b
Benedito Carvalho <benedito.carvalho@gmail.com>
2021-10-08 11:48:33 -0300

    Novidades no Java 11

---
+++ src/Aula6/CollectionExemplo.java
@@ -0,0 +1,13 @@
+package Aula6;
+
+import java.util.Collection;
+import java.util.Set;
+
+public class CollectionExemplo {
+
+    public static void main(String[] args) {
+        Collection<String> nomes = Set.of("Benedito", "José", "Carvalho", "Nunes");
+
+        System.out.println(nomes);
+    }
+}
---
+++ src/Aula6/StringRepeat.java
@@ -0,0 +1,8 @@
+package Aula6;
+
+public class StringRepeat {
+    public static void main(String[] args) {
+        String nome = "Benedito";
+        System.out.println(nome.repeat(10));
+    }
+}
